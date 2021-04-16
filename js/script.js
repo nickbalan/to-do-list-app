4842b18f76da2f2b518b9bfe6fc483c33ed459de
nickbalan <nickolae.balan@gmail.com>
2021-04-16 13:12:18 +0200

    This commit has created li element with jQuery.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* 1. Adding a new item to the list of items: */
+/* Javascript code: */
+// function newItem(){
+	// let li = document.createElement("li");
+/* jQuery code: */
+function newItem(){
+	/* Creates the li element with jQuery. */
+	let li = $('<li></li>');
+};
